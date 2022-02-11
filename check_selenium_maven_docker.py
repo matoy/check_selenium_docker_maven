9cb978fa649473dc94eca9c838b62cc5af3ae42c
matoy <matoy@users.noreply.github.com>
2022-02-11 12:36:45 +0100

    Add files via upload

--- check_selenium_maven_docker.py
+++ check_selenium_maven_docker.py
@@ -20,6 +20,7 @@ import json
 import argparse
 import glob
 import re
+import xmltodict
 
 # catch-all for exceptions exit codes
 # @see https://nagios-plugins.org/doc/guidelines.html#AEN78
@@ -33,49 +34,41 @@ sys.excepthook = except_hook
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--verbose', action='count', default=0,
                     help="show failed test names and failure messages (-vv)")
+parser.add_argument("--debug", type=str, default="false",
+                    help="expose 4444 and 7900 ports of the container to the localhost to share screen/browser view, default 'false', other possible options: 'true'")
 parser.add_argument("--timeout", type=int, default=300,
                     help="results waiting timeout in sec, default 300")
 parser.add_argument("--browser", type=str, default="chrome",
-                    help="container version to use, default 'chrome', other possible options: 'firefox' or 'edge'")
-parser.add_argument("--gridproto", type=str, default="http",
-                    help="selenium grid protocol to use, default 'http', other possible option: 'https'")
+                    help="container version to use, default 'chrome', other possible options: 'firefox' or 'edge', only used to determine which selenium should be executed ; you should also specify the browser to use in your java code")
 parser.add_argument("--gridfqdn", type=str, default="localhost",
-                    help="selenium grid server to use, default 'localhost', other possible options: 'fqdn of your selenium grid server'")
-parser.add_argument("--gridport", type=str, default="4444",
-                    help="selenium grid server port to use, default '4444', other possible options: 'port of your selenium grid server'")
+                    help="selenium grid server to use, default 'localhost', only used to determine if the selenium should be executed in the containers background ; you should also specify the grid hub to use in your java code")
 parser.add_argument('--no-newlines', action='store_true',
                     help="print newlines literally on multiline output")
-parser.add_argument("path", type=str, help="path to selenium test")
+parser.add_argument("--path", type=str, default="/usr/lib/centreon/plugins/selenium/junit-selenium-sample", help="path to selenium/maven resources")
+parser.add_argument("--mavenphase", type=str, default="test", help="your maven phase name")
+parser.add_argument("--mavenenv", type=str, default="prod", help="your maven env to test: dev, uat, prod, etc.")
+parser.add_argument("--mavenscenario", type=str, default="myscenario", help="your maven scenario name")
+parser.add_argument("--mavenlocale", type=str, default="fr_fr", help="your maven locale")
+parser.add_argument("--mavenreport", type=str, default="surefire-reports/TEST-com.lambdatest.MyClass.xml", help="your target folder related path to xml report")
 args = parser.parse_args()
 path = args.path
 browser = args.browser
-gridproto = args.gridproto
 gridfqdn = args.gridfqdn
-gridport = args.gridport
+mavenphase = args.mavenphase
+mavenenv = args.mavenenv
+mavenscenario = args.mavenscenario
+mavenlocale = args.mavenlocale
+mavenreport = args.mavenreport
 timeout = abs(args.timeout)
+debug = args.debug
 verbose = args.verbose
 os.chdir(path)
 if browser not in ['chrome', 'firefox', 'edge']:
     print("Error: not allowed browser!")
     sys.exit(3)
 
-# Count projects for results observe
-projects = []
-for side in glob.glob(path + '/sides/*.side'):
-    side_file = open(side,'r')
-    side_json = json.loads(side_file.read())
-    side_file.close()
-    if side_json['name'] in projects:
-        print("Error: duplicated project names! Check input side files.")
-        sys.exit(3)
-    projects.append(side_json['name'])
-projectsNb = len(projects)
-if projectsNb == 0:
-    print("Error: no valid input side files found!")
-    sys.exit(3)
-
-# Remove old result json files
-for result in glob.glob(path + '/out/*.json'):
+# Remove old result xml repotr files
+for result in glob.glob(path + '/out/' + mavenenv + '-' + mavenlocale + '-' + mavenscenario + '.xml'):
     os.remove(result)
 
 # Signal trap for container cleaning.
@@ -88,22 +81,26 @@ for sig in [signal.SIGTERM, signal.SIGINT]:
     signal.signal(sig, handler)
 
 # Start selenium docker container
+ports = {}
+if debug == "true":
+    ports = {'4444/tcp': 4444, '7900/tcp': 7900}
 client = docker.from_env()
 container = client.containers.run(
-    'opsdis/selenium-' + browser + '-node-with-side-runner',
+    'selenium-' + browser + '-node-with-maven',
     auto_remove = True,
     shm_size = '2G',
     volumes = {
-        path + '/out': { 'bind': '/selenium-side-runner/out', 'mode': 'rw' },
-        path + '/sides': { 'bind': '/sides', 'mode': 'rw' },
+        path + '/': { 'bind': '/maven', 'mode': 'rw' },
     },
-    environment={'GRID_FQDN':gridfqdn, 'GRID_PORT':gridport, 'GRID_PROTO':gridproto},
+    environment={'GRID_FQDN':gridfqdn, 'MAVEN_phase':mavenphase, 'MAVEN_environment':mavenenv, 'MAVEN_cucumberFilterTags':mavenscenario, 'MAVEN_locale':mavenlocale, 'MAVEN_reportxmlfile':mavenreport},
+    ports = ports,
     detach = True
 )
 
 # Wait for result file to be written
 waitedfor = 0
-while len(glob.glob(path + '/out/*.json')) < projectsNb and waitedfor <= timeout:
+projectsNb = 1
+while len(glob.glob(path + '/out/' + mavenenv + '-' + mavenlocale + '-' + mavenscenario + '.xml')) < projectsNb and waitedfor <= timeout:
     time.sleep(1)
     waitedfor += 1
 
@@ -119,117 +116,37 @@ if waitedfor >= timeout:
     sys.exit(3)
 
 # Parse result files
-times = { 'startTime': 0, 'endTime': 0 }
-json_input = { 'num' + i: 0 for i in
-    [ 'FailedTestSuites', 'PassedTests', 'FailedTests', 'TotalTests']
-}
-failed = {}
-for result in glob.glob(path + '/out/*.json'):
-    file = open(result,'r')
-    jsonResult = json.loads(file.read())
-    file.close()
-    if times['startTime'] == 0 or times['startTime'] > jsonResult['startTime']:
-        times['startTime'] = jsonResult['startTime']
-    for par in json_input.keys():
-        json_input[par] += jsonResult[par]
-    for testResult in jsonResult['testResults']:
-        if times['endTime'] < testResult['endTime']:
-            times['endTime'] = testResult['endTime']
-        for aResult in testResult['assertionResults']:
-            if aResult['status'] == 'failed':
-                failed[aResult['fullName']] = aResult['failureMessages']
-
-# Calculate execution time
-exec_time = 0 if times['endTime'] <= times['startTime'] else \
-    int(round(float(times['endTime'] - times['startTime'])/1000))
+# format:
+# <testsuite tests="1" failures="0" name="com.lambdatest.IntranetchristiandiorcoutureTest" time="6.568" errors="0" skipped="0">
+#with open('/usr/lib/centreon/plugins/selenium/java/junit-selenium-sample/out/single-fr_fr-@account_01.xml', 'r') as myfile:
+with open(path + '/out/' + mavenenv + '-' + mavenlocale + '-' + mavenscenario + '.xml', 'r') as myfile:
+    obj = xmltodict.parse(myfile.read())
+testsNb = int(obj['testsuite']['@tests'])
+failuresNb = int(obj['testsuite']['@failures'])
+errorsNb = int(obj['testsuite']['@errors'])
+skippedNb = int(obj['testsuite']['@skipped'])
+passedNb = int(testsNb - failuresNb - errorsNb - skippedNb)
+from decimal import Decimal
+exec_time = Decimal(obj['testsuite']['@time'])
+testcaseClassname = obj['testsuite']['testcase']['@classname']
+testcaseName = obj['testsuite']['testcase']['@name']
 
 # Performace Data
 perfData = ("'passed'={1};;{0}:;0;{0} 'failed'={2};;~:0;0;{0} 'exec_time'={3}s"
-    ';;;;').format(json_input['numTotalTests'], json_input['numPassedTests'],
-                   json_input['numFailedTests'], exec_time)
-# Perf data from console output
-# @see https://nagios-plugins.org/doc/guidelines.html#THRESHOLDFORMAT
-def outOfRange(val, thre):
-    val = float(val)
-    if ':' not in thre:
-        return val < 0 or val > float(thre)
-    thre = thre.split(':')
-    if thre[0] == '~':
-        return val > float(thre[1])
-    if thre[1] == '':
-        return val < float(thre[0])
-    if '@' in thre[0]:
-        return val >= float(thre[0].replace('@', '')) and val <= float(thre[1])
-    return val < float(thre[0]) or val > float(thre[1])
+    ';;;;').format(testsNb, passedNb, failuresNb, exec_time)
 
 statusCode = 0
 exitStatus = ['OK', 'WARNING', 'CRITICAL']
-# [warn, crit]
-alerts = [[], []]
-if os.path.isfile(path + '/out/output.log'):
-    perfDataReg = ("PERFDATA: '?([^=']+[^' ])'? *= *"
-                   "((-?[0-9]+(\.[0-9]+)?|U)[^\d';]*(;.*)*)\n$")
-    # [threshold, min/max]
-    validators = [
-        re.compile('^(~|@?[0-9]+(\.[0-9]+)?)(:|:[0-9]+(\.[0-9]+)?)?$'),
-        re.compile('^[0-9]+(\.[0-9]+)?$'),
-    ]
-    file = open(path + '/out/output.log','r')
-    for perf in re.findall(perfDataReg, file.read(), flags=re.MULTILINE):
-        perfVals = perf[1].split(';')
-        perfValid = True
-        # validate thresholds and min/max fields
-        for id in range(1,5):
-            if len(perfVals) > id and perfVals[id] != '' \
-                    and not validators[0 if id < 3 else 1].match(perfVals[id]):
-               perfValid = False
-        if len(perfVals) > 5 or not perfValid:
-            continue
-        # compare value with crit and warn thresholds
-        for id in [2, 1]:
-            if perf[2] != 'U' and len(perfVals) > id and perfVals[id] != '' \
-                    and outOfRange(perf[2], perfVals[id]):
-                if statusCode < id:
-                    statusCode = id
-                # do not put warn alert if crit exits
-                if id != 1 or perf[0] not in alerts[1]:
-                    alerts[id - 1].append(perf[0])
-        perfData += " '{}'={}".format(*perf)
-    file.close()
-
-def getAlertsInfo():
-    alertsInfo = [
-        '{} critical and {} warning alerts.'.format(*map(len, reversed(alerts)))
-    ]
-    if verbose > 0:
-        for idx, val in enumerate(alerts):
-            if len(val) > 0:
-                alertsInfo.append('{}: {}.'.format(exitStatus[idx + 1].title(),
-                    ', '.join(val)))
-    return ' '.join(alertsInfo)
 
 # Exit logic with performance data
-if json_input['numFailedTestSuites'] == 0 and json_input['numFailedTests'] == 0:
-    print('{}: Passed {} of {} tests'.format(exitStatus[statusCode],
-            json_input['numPassedTests'], json_input['numTotalTests']) +
-       ('.' if statusCode == 0 else ' with ' + getAlertsInfo()) +
-       " | " + perfData)
-    sys.exit(statusCode)
+if failuresNb == 0 and errorsNb == 0:
+    print('OK: Passed {} of {} tests in {}s ({}/{}).'.format(passedNb, testsNb, exec_time, testcaseClassname, testcaseName) + " | " + perfData)
+    sys.exit(0)
 
-elif json_input['numFailedTests'] > 0:
-    print('CRITICAL: Failed {} of {} tests'.format(json_input['numFailedTests'],
-            json_input['numTotalTests']) +
-      ('.' if verbose == 0 else ": " +  ', '.join(failed.keys()) + '.' ) +
-      ('' if not sum(map(len, alerts)) else ' ' + getAlertsInfo()) +
-      " | " + perfData, end = '')
-    if verbose > 1:
-        for testName in failed:
-            output = '\nTEST: ' + testName + '\n' + '\n\n'.join(failed[testName])
-            print(output.replace('\n', '\\n') if args.no_newlines else output,
-                  end = '')
-    print('')
+elif failuresNb > 0:
+    print('CRITICAL: Failed {} and Error {} of {} tests ({}/{}).'.format(failuresNb, errorsNb, testsNb, testcaseClassname, testcaseName) + " | " + perfData)
     sys.exit(2)
 
 else:
-    print("UNKNOWN: Investigate issues")
-    sys.exit(3)
+    print("CRITICAL: Investigate issues")
+    sys.exit(2)
